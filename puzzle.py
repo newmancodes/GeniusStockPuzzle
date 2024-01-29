b0e1cd6512520f945304b6dadc4985ff220220a4
Stephen Newman <steve@newman.digital>
2024-01-29 19:12:36 +0000

    Done.

---
+++ puzzle.py
@@ -0,0 +1,21 @@
+def maxProfitStrategy(prices: [int]) -> (int, int):
+    l, r = 0 ,1
+    maxProfit = 0
+
+    while r < len(prices):
+        if prices[l] < prices[r]:
+            profit = prices[r] - prices[l]
+            maxProfit = max(maxProfit, profit)
+            if maxProfit == profit:
+                maxProfitL = l
+                maxProfitR = r
+        else:
+            l = r
+    
+        r += 1
+
+    return (maxProfitL, maxProfitR)
+
+prices = [5, 9, 3, 8, 1, 7]
+strategy = maxProfitStrategy(prices)
+print(f'maximum profit possible for {prices} by buying at {prices[strategy[0]]} and selling at {prices[strategy[1]]}, yielding a profit of {prices[strategy[1]] - prices[strategy[0]]}.\n')
