b411536bc5016d3ee8d175a5d3496476627cd5b8
Stephen Newman <steve@newman.digital>
2024-01-31 07:31:58 +0000

    Add additional examples.

--- puzzle.py
+++ puzzle.py
@@ -1,6 +1,7 @@
 def maxProfitStrategy(prices: [int]) -> (int, int):
-    l, r = 0 ,1
+    l, r = 0, 1
     maxProfit = 0
+    maxProfitL, maxProfitR = l, r
 
     while r < len(prices):
         if prices[l] < prices[r]:
@@ -16,6 +17,10 @@ def maxProfitStrategy(prices: [int]) -> (int, int):
 
     return (maxProfitL, maxProfitR)
 
-prices = [5, 9, 3, 8, 1, 7]
-strategy = maxProfitStrategy(prices)
-print(f'maximum profit possible for {prices} by buying at {prices[strategy[0]]} and selling at {prices[strategy[1]]}, yielding a profit of {prices[strategy[1]] - prices[strategy[0]]}.\n')
+def process(prices: [int]):
+    strategy = maxProfitStrategy(prices)
+    print(f'maximum profit possible for {prices} by buying at {prices[strategy[0]]} and selling at {prices[strategy[1]]}, yielding a profit of {prices[strategy[1]] - prices[strategy[0]]}.\n')
+
+process([5, 9, 3, 8, 1, 7])
+process([3, 6, 1, 6])
+process([3, 6, 1, 5])
